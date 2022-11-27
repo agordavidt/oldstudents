620f4c270fcab7d1ced5c99a019e2229e1210654
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-11-27 02:48:42 +0100

    requirements.txt created

--- fedvosa/fedvosa/settings.py
+++ fedvosa/fedvosa/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vmy#g-3nd=1tstg&(h7zrb(6@i!gto#wt3y&m4c@6(o+gw#wkj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
